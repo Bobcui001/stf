2382723822b4a8782a45d84857728fbd2e6258d7
B.O.B <bob.cui@yunzhihui.com>
2017-03-02 20:32:35 +0800

    1. npm pacakges 安装 2. 忽略文件增加 logs code review by self

--- res/auth/mock/scripts/signin/signin-controller.js
+++ res/auth/mock/scripts/signin/signin-controller.js
@@ -1,7 +1,11 @@
 module.exports = function SignInCtrl($scope, $http) {
+  console.info('TEST');
+  return false;
   $scope.error = null
 
   $scope.submit = function() {
+    console.info('TEST2');
+    return false;
     var data = {
       name: $scope.signin.username.$modelValue
       , email: $scope.signin.email.$modelValue
@@ -9,8 +13,9 @@ module.exports = function SignInCtrl($scope, $http) {
     $scope.invalid = false
     $http.post('/auth/api/v1/mock', data)
       .success(function(response) {
-        $scope.error = null
-        location.replace(response.redirect)
+        $scope.error = null,
+        console.info('TEST');
+        // location.replace(response.redirect)
       })
       .error(function(response) {
         switch (response.error) {
